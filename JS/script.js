d273dc2c483d4892a671c085804efefc8d33e6e5
AnasNAK <anasc00ding@gmail.com>
2024-07-02 10:08:37 +0100

    create the script js

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addTaskButton = document.getElementById('addTaskButton');
+    const removeTaskButton = document.getElementById('removeTaskButton');
+    const highlightTaskButton = document.getElementById('highlightTaskButton');
+    const myList = document.getElementById('myList');
+
+    addTaskButton.addEventListener('click', function() {
+        const newTask = document.createElement('li');
+        newTask.textContent = 'Nouvelle tâche';
+        myList.appendChild(newTask);
+    });
+
+    removeTaskButton.addEventListener('click', function() {
+        const lastTask = myList.lastElementChild;
+       
+            myList.removeChild(lastTask);
+        
+    });
+
+    highlightTaskButton.addEventListener('click', function() {
+        const firstTask = myList.firstElementChild;
+
+            const tasks = myList.querySelectorAll('li');
+            tasks.forEach(task => task.style.backgroundColor = '');
+            firstTask.style.backgroundColor = 'yellow';
+ 
+    });
+});
