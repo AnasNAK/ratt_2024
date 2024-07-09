5cf6efa878a4d7c4a7463c9a4f48106b8d51306b
AnasNAK <anasc00ding@gmail.com>
2024-07-09 11:07:47 +0100

    finall update

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
     const addTaskButton = document.getElementById('addTaskButton');
     const removeTaskButton = document.getElementById('removeTaskButton');
     const highlightTaskButton = document.getElementById('highlightTaskButton');
     const myList = document.getElementById('myList');
 
-    addTaskButton.addEventListener('click', function() {
+    addTaskButton.addEventListener('click', function () {
         const newTask = document.createElement('li');
         newTask.textContent = 'Nouvelle tâche';
         myList.appendChild(newTask);
     });
 
-    removeTaskButton.addEventListener('click', function() {
+    removeTaskButton.addEventListener('click', function () {
         const lastTask = myList.lastElementChild;
-       
-            myList.removeChild(lastTask);
-        
+
+        myList.removeChild(lastTask);
+
     });
 
-    highlightTaskButton.addEventListener('click', function() {
+    highlightTaskButton.addEventListener('click', function () {
         const firstTask = myList.firstElementChild;
 
-            const tasks = myList.querySelectorAll('li');
-            tasks.forEach(task => task.style.backgroundColor = '');
-            firstTask.style.backgroundColor = 'yellow'; 
+        firstTask.style.backgroundColor = 'yellow';
 
     });
-});
+
